3775c37ab0f2c810f25c9cf040dcd0737011fdee
Vera <contact.vera0011@gmail.com>
2022-04-18 09:24:22 +0200

    Resolved exportation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // BIOMES
-const findBiomeById = (languaje, id) => {
+export const findBiomeById = (languaje, id) => {
     const biomes = require(`./${languaje}/biomes.js`);
     const biomeId = Object.keys(biomes).find((biomeId) => biomeId === id);
 
     return biomes[biomeId] || 0;
 }
 
-const findBiomeByName = (languaje, name) => {
+export const findBiomeByName = (languaje, name) => {
     const biomes = require(`./${languaje}/biomes`);
     const biome = Object.values(biomes).find((biome) => biome.name === name);
 
@@ -14,14 +14,14 @@ const findBiomeByName = (languaje, name) => {
 }
 
 // BLOCKS
-const findBlockById = (languaje, id) => {
+export const findBlockById = (languaje, id) => {
     const blocks = require(`./${languaje}/blocks.js`);
     const blockId = Object.keys(blocks).find((blockId) => blockId === id);
 
     return blocks[blockId] || 0;
 }
 
-const findBlockByName = (languaje, name) => {
+export const findBlockByName = (languaje, name) => {
     const blocks = require(`./${languaje}/blocks.js`);
     const block = Object.values(blocks).find((block) => block.name === name);
 
@@ -29,14 +29,14 @@ const findBlockByName = (languaje, name) => {
 }
 
 // EFFECTS
-const findEffectById = (languaje, id) => {
+export const findEffectById = (languaje, id) => {
     const effects = require(`./${languaje}/effects.js`);
     const effectId = Object.keys(effects).find((effectId) => effectId === id);
 
     return effects[effectId] || 0;
 }
 
-const findEffectByName = (languaje, name) => {
+export const findEffectByName = (languaje, name) => {
     const effects = require(`./${languaje}/effects.js`);
     const effect = Object.values(effects).find((effect) => effect.name === name);
 
@@ -44,14 +44,14 @@ const findEffectByName = (languaje, name) => {
 }
 
 // ENCHANTS
-const findEnchantById = (languaje, id) => {
+export const findEnchantById = (languaje, id) => {
     const enchants = require(`./${languaje}/enchants.js`);
     const enchantId = Object.keys(enchants).find((enchantId) => enchantId === id);
 
     return enchants[enchantId] || 0;
 }
 
-const findEnchantByName = (languaje, name) => {
+export const findEnchantByName = (languaje, name) => {
     const enchants = require(`./${languaje}/enchants.js`);
     const enchant = Object.values(enchants).find((enchant) => enchant.name === name);
 
@@ -59,14 +59,14 @@ const findEnchantByName = (languaje, name) => {
 }
 
 // ITEMS
-const findItemById = (languaje, id) => {
+export const findItemById = (languaje, id) => {
     const items = require(`./${languaje}/items.js`);
     const itemId = Object.keys(items).find((itemId) => itemId === id);
 
     return items[itemId] || 0;
 }
 
-const findItemByName = (languaje, name) => {
+export const findItemByName = (languaje, name) => {
     const items = require(`./${languaje}/items.js`);
     const item = Object.values(items).find((item) => item.name === name);
 
@@ -74,14 +74,14 @@ const findItemByName = (languaje, name) => {
 }
 
 // MOBS
-const findMobById = (languaje, id) => {
+export const findMobById = (languaje, id) => {
     const mobs = require(`./${languaje}/mobs.js`);
     const mobId = Object.keys(mobs).find((mobId) => mobId === id);
 
     return mobs[mobId] || 0;
 }
 
-const findMobByName = (languaje, name) => {
+export const findMobByName = (languaje, name) => {
     const mobs = require(`./${languaje}/mobs.js`);
     const mob = Object.values(mobs).find((mob) => mob.name === name);
 
@@ -89,33 +89,16 @@ const findMobByName = (languaje, name) => {
 }
 
 // STRUCTURES
-const findStructureById = (languaje, id) => {
+export const findStructureById = (languaje, id) => {
     const structures = require(`./${languaje}/structures.js`);
     const structureId = Object.keys(structures).find((structureId) => structureId === id);
 
     return structures[structureId] || 0;
 }
 
-const findStructureByName = (languaje, name) => {
+export const findStructureByName = (languaje, name) => {
     const structures = require(`./${languaje}/structures.js`);
     const structure = Object.values(structures).find((structure) => structure.name === name);
 
     return structure || 0;
-}
-
-module.exports = {
-    findBiomeById,
-    findBiomeByName,
-    findBlockById,
-    findBlockByName,
-    findEffectById,
-    findEffectByName,
-    findEnchantById,
-    findEnchantByName,
-    findItemById,
-    findItemByName,
-    findMobById,
-    findMobByName,
-    findStructureById,
-    findStructureByName
 }
